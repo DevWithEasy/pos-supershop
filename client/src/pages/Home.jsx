47eddc58a13acc7aa369d8d07d168ed9575e7e99
DevWithEasy <devwitheasy@gmail.com>
2023-11-09 16:43:01 +0600

    Home page ui clean

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 const Home = () => {
 
     return (
-        <div className='p-2'>
-            <h1 className='text-4xl text-center'>Wellcome</h1>
+        <div className='h-screen p-2 flex justify-center items-center'>
+            <div>
+                <h1 className='text-6xl font-bold text-center text-sky-500'>Home Plus</h1>
+                <p className='text-center text-gray-500'>Buy your all daily needs.</p>
+                <hr/>
+            </div>
         </div>
     );
 };
